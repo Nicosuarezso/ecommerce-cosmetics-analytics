b3ab7653872496bc2213dbf35cefd46d77ab48b8
Nicolas <nicosuarezsomp@gmail.com>
2026-09-06 20:27:05 -0400

    notebooks:
    04_customer_analysis: Se concluyo el analisis de carrito nocturno 3.3 y se agregaron las conclusiones e insights mas importantes al final de la seccion 3.3
    
    src/analysis: Se agrego una funcion para calcular la compra post noche

--- src/analysis/customers.py
+++ src/analysis/customers.py
@@ -1,1 +1,18 @@
 """Análisis de clientes."""
+def calculate_post_night_purchase(df_check, hours):
+    
+    condition = (
+        (df_check["hours_to_purchase"] > 0) &
+        (df_check["hours_to_purchase"] <= hours)
+    )
+    
+    result = (
+        df_check
+        .assign(
+            purchase_after_night=condition
+        )
+        .groupby("user_id")["purchase_after_night"]
+        .any()
+    )
+    
+    return result
